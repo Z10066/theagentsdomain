a84afd4e3aa7d3127c21059fdf3902062679e65d
wp8561hahaha <wp8561@gmail.com>
2024-04-09 14:14:13 +0900

    20240409-02-wp

---
+++ 03.ClientPlatform/04.Animation/src/main/webapp/app/home/continue_page/continue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-continue',
+  standalone: true,
+  imports: [],
+  templateUrl: './continue.html',
+  styleUrl: './promptSelectionStyles.scss'
+})
+export class ContinueComponent {
+
+}
