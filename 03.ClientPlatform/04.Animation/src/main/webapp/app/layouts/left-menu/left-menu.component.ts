64729c420546e923b2005643e5054b1954856e1d
wp8561hahaha <wp8561@gmail.com>
2024-04-16 14:38:27 +0900

    20240416-3--wp

--- 03.ClientPlatform/04.Animation/src/main/webapp/app/layouts/left-menu/left-menu.component.ts
+++ 03.ClientPlatform/04.Animation/src/main/webapp/app/layouts/left-menu/left-menu.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import SharedModule from 'app/shared/shared.module';
 
 @Component({
@@ -9,18 +9,30 @@ import SharedModule from 'app/shared/shared.module';
   templateUrl: './left-menu.component.html',
   styleUrl: './left-menu.component.scss'
 })
-export class LeftMenuComponent {
-
+export class LeftMenuComponent implements OnInit {
+@Input()  currentpage:string = ""
   navItems = [
-    { label: 'Workspaces', link: '/Workspaces', target: '_self' },
-    { label: 'Createvideoprompt', link: '/Createvideoprompt', target: '_blank' },
-    { label: 'Members', link: '/Members', target: '_self' },
-    { label: 'Plan', link: '/Plan', target: '_self' },
-    { label: 'Usage', link: '/Usage', target: '_self' },
-    { label: 'VideoProduct', link: '/VideoProduct', target: '_self' },
-    { label: 'Material', link: '/Material', target: '_self' },
-    { label: 'History', link: '/History', target: '_self' },
-    { label: 'Profile', link: '/Profile', target: '_self' },
+    { label: 'Workspaces', link: '/Workspaces', target: '_self', active: false },
+    { label: 'Createvideoprompt', link: '/Createvideoprompt', target: '_blank', active: false },
+    { label: 'Members', link: '/Members', target: '_self', active: false },
+    { label: 'Plan', link: '/Plan', target: '_self', active: false },
+    { label: 'Usage', link: '/Usage', target: '_self', active: false },
+    { label: 'VideoProduct', link: '/VideoProduct', target: '_self', active: false },
+    { label: 'Material', link: '/Material', target: '_self', active: false },
+    { label: 'History', link: '/History', target: '_self', active: false },
+    { label: 'Profile', link: '/Profile', target: '_self', active: false },
   ];
+
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+    this.navItems.forEach(navItem => {
+      if ( navItem.label == this.currentpage )
+        navItem.active = true; 
+      else navItem.active = false;
+    });
+  }
 
 }
