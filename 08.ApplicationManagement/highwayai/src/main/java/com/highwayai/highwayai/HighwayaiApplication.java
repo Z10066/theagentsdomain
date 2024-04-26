d927a778c0c441c8e86f498d4124789886f46efb
Z10066 <z1042331515@gmail.com>
2024-04-26 18:20:38 +0900

    20240426Z

---
+++ 08.ApplicationManagement/highwayai/src/main/java/com/highwayai/highwayai/HighwayaiApplication.java
@@ -0,0 +1,13 @@
+package com.highwayai.highwayai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighwayaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighwayaiApplication.class, args);
+	}
+
+}
---
+++ 08.ApplicationManagement/highwayai/src/test/java/com/highwayai/highwayai/HighwayaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highwayai.highwayai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighwayaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
