c08ea8386617d5263e87b508cae045ff90c927e6
Z10066 <z1042331515@gmail.com>
2024-05-22 16:07:56 +0900

    20240522

---
+++ 03.ClientPlatform/04.Animation/src/main/webapp/app/home/inviteusers/inviteusers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-inviteusers',
+  standalone: true,
+  imports: [],
+  templateUrl: './inviteusers.component.html',
+  styleUrl: './inviteusers.component.scss'
+})
+export class InviteusersComponent {
+
+}
